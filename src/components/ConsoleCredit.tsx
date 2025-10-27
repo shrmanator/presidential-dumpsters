08c47e4c8c37452f2828da3778566e1b1ff5d9f2
Dov <dovindustries@gmail.com>
2025-10-26 22:09:21 -0400

    refactor: Update console ASCII art to cleaner style
    Replace block-style ASCII art with cleaner standard ASCII art for
    dovindustries branding in browser console.

--- src/components/ConsoleCredit.tsx
+++ src/components/ConsoleCredit.tsx
@@ -5,8 +5,15 @@ import { useEffect } from 'react';
 export function ConsoleCredit() {
   useEffect(() => {
     console.log(
-      '%c dovindustries.com ',
-      'background: #000; color: #fff; font-size: 14px; padding: 4px 8px; font-family: monospace;'
+      '%c\n' +
+      '     _               _             _              _          _            \n' +
+      '  __| |  ___ __   __(_) _ __    __| | _   _  ___ | |_  _ __ (_)  ___  ___ \n' +
+      ' / _` | / _ \\\\ \\ / /| || \'_ \\  / _` || | | |/ __|| __|| \'__|| | / _ \\/ __|\n' +
+      '| (_| || (_) |\\ V / | || | | || (_| || |_| |\\__ \\| |_ | |   | ||  __/\\__ \\\n' +
+      ' \\__,_| \\___/  \\_/  |_||_| |_| \\__,_| \\__,_||___/ \\__||_|   |_| \\___||___/\n' +
+      '\n%c dovindustries.com \n',
+      'font-family: monospace; color: #fff;',
+      'font-family: monospace; color: #888; font-size: 12px;'
     );
   }, []);
 
