1e83f80ec190808a92547f45665aa6bd320e94a0
Dov <dovindustries@gmail.com>
2025-10-30 13:49:05 -0400

    Add Microsoft Clarity session recording integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -250,6 +250,19 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
         />
+        {/* Microsoft Clarity */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID || 'YOUR_PROJECT_ID'}");
+            `,
+          }}
+        />
       </head>
       <body
         className={`${inter.variable} font-sans antialiased`}
